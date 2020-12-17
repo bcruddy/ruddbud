8e337f7632ee0e07a459d122b348c99afc594b41
brian ruddy <briancruddy@gmail.com>
2020-12-17 07:48:56 -0500

    script init for iframe

--- dev/init.js
+++ dev/init.js
@@ -1,2 +1,12 @@
 ruddbud.init('dev');
 ruddbud.appendDynamicEl();
+
+if (window.location.pathname.includes('iframe')) {
+    setTimeout(() => {
+        window.pendo.track('kitties-500');
+    }, 500);
+
+    setTimeout(() => {
+        window.pendo.track('kitties-1500');
+    }, 1500);
+}
