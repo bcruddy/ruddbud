fba6375a3c882450b0dd88c688ed9d98b8e25fe2
brian ruddy <briancruddy@gmail.com>
2021-01-07 08:39:49 -0500

    add cross-origin iframe

--- dev/init.js
+++ dev/init.js
@@ -1,12 +1,46 @@
 (function init () {
     const isTopFrame = window === window.top;
+    const params = parseQueryParams();
+    console.log(window.location.pathname, params)
 
     if (isTopFrame) {
         ruddbud.appendDynamicEl();
+
         const aboutDoc = document.getElementById('about-blank').contentDocument;
         aboutDoc.write('<h1>about:blank!</h1><p>no agent</p>');
         aboutDoc.close();
+
+        if (params.domainmismatch) {
+            const iframe = document.createElement('iframe');
+
+            iframe.setAttribute('id', 'domain-mismatch');
+            iframe.setAttribute('src', 'https://dev.local:3000/dev/iframe-other-cat-articles.html');
+
+            const grid = document.querySelector('.frame-grid');
+
+            grid && grid.appendChild(iframe);
+        }
     }
 
+
+
     ruddbud.init('dev', { hideNav: !isTopFrame });
+
+
+    function parseQueryParams () {
+        const { search } = window.location;
+
+        if (!search) {
+            return {};
+        }
+
+        return search.replace('?', '').split('&').reduce((map, kv) => {
+            const [key, value] = kv.split('=');
+
+            return {
+                ...map,
+                [key]: value
+            };
+        }, {})
+    }
 }());
